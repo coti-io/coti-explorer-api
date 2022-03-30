40409c51ff1d3ae758c1a8342006ed965f3c5427
Yuval <yuval@coti.io>
2022-03-30 20:04:57 +0300

    removed * cors

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,5 +1,5 @@
 export const corsConfig = {
-  origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:3006', 'https://explorer-qa.coti.io', 'https://explorer.coti.io', '*'],
+  origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:3006', 'https://explorer-qa.coti.io', 'https://explorer.coti.io'],
   credentials: true,
 };
 
