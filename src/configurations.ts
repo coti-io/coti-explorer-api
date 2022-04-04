1847a20c0c65ee0f277d6db907b439ae9ac0319b
cotitech <eli@coti.io>
2022-04-04 13:39:51 +0300

    staging added to cors

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,5 +1,12 @@
 export const corsConfig = {
-  origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:3006', 'https://explorer-qa.coti.io', 'https://explorer.coti.io'],
+  origin: [
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'http://localhost:3006',
+    'https://explorer-qa.coti.io',
+    'https://explorer-staging.coti.io',
+    'https://explorer.coti.io',
+  ],
   credentials: true,
 };
 
